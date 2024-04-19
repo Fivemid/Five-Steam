bc191a06e54062ef9ef252e377bb309de3b36a6d
Marc Ducret <marc.t.ducret@gmail.com>
2024-04-19 15:47:44 +0200

    SteamNetworkingSendFlags.

--- Fivemid.FiveSteam.Generator~/Writer/CodeWriter.Constant.cs
+++ Fivemid.FiveSteam.Generator~/Writer/CodeWriter.Constant.cs
@@ -10,28 +10,29 @@ public partial class CodeWriter {
         TypeDeclaration(SyntaxKind.StructDeclaration, "FiveSteamConstants")
            .AddModifiers(Token(SyntaxKind.PublicKeyword))
            .AddMembers(definitions.Select(Constant).ToArray<MemberDeclarationSyntax>());
-
+    
     private FieldDeclarationSyntax Constant(SteamApiDefinition.Const definition) {
-        ExpressionSyntax initializer;
-
-        if (constantInitializerOverrides.TryGetValue(definition.Name, out string? initializerString)) {
-            initializer = ParseExpression(initializerString);
-        } else {
-            initializer = definition.Value.ToInitializerValue();
-        }
-
+        ExpressionSyntax initializer =
+            constantInitializerOverrides.TryGetValue(definition.Name, out string? initializerString)
+                ? ParseExpression(initializerString)
+                : definition.Value.ToInitializerValue();
+        
+        TypeSyntax type = definition.Type.ToType();
         return FieldDeclaration(
-                VariableDeclaration(definition.Type.ToType())
+                VariableDeclaration(type)
                    .AddVariables(VariableDeclarator(definition.Name)
                                     .WithInitializer(
                                          EqualsValueClause(initializer)))
-            ).AddModifiers(Token(SyntaxKind.PublicKeyword),
-                           Token(SyntaxKind.StaticKeyword),
-                           Token(SyntaxKind.ReadOnlyKeyword))
-             .WithLeadingTrivia(Comment($"/// <summary>{definition.Name}</summary>")); // TODO sometimes use const
+            ).AddModifiers(Token(SyntaxKind.PublicKeyword))
+             .AddModifiers(
+                  definition.Type.IsPrimitive()
+                      ? [Token(SyntaxKind.ConstKeyword)]
+                      : [Token(SyntaxKind.StaticKeyword), Token(SyntaxKind.ReadOnlyKeyword)]
+              )
+             .WithLeadingTrivia(Comment($"/// <summary>{definition.Name}</summary>"));
     }
-
-    private static Dictionary<string, string> constantInitializerOverrides = new() {
+    
+    private static readonly Dictionary<string, string> constantInitializerOverrides = new() {
         { "HSERVERQUERY_INVALID", "unchecked((int)0xffffffff)" },
         { "k_SteamItemInstanceIDInvalid", "~0ul" },
         { "k_SteamDatagramPOPID_dev", "((uint)'d' << 16) | ((uint)'e' << 8) | (uint)'v'" }
---
+++ Fivemid.FiveSteam/SteamNetworkingSendFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fivemid.FiveSteam {
+    [Flags]
+    public enum SteamNetworkingSendFlags : int {
+        Unreliable               = FiveSteamConstants.k_nSteamNetworkingSend_Unreliable,
+        NoNagle                  = FiveSteamConstants.k_nSteamNetworkingSend_NoNagle,
+        UnreliableNoNagle        = FiveSteamConstants.k_nSteamNetworkingSend_UnreliableNoNagle,
+        NoDelay                  = FiveSteamConstants.k_nSteamNetworkingSend_NoDelay,
+        UnreliableNoDelay        = FiveSteamConstants.k_nSteamNetworkingSend_UnreliableNoDelay,
+        Reliable                 = FiveSteamConstants.k_nSteamNetworkingSend_Reliable,
+        ReliableNoNagle          = FiveSteamConstants.k_nSteamNetworkingSend_ReliableNoNagle,
+        UseCurrentThread         = FiveSteamConstants.k_nSteamNetworkingSend_UseCurrentThread,
+        AutoRestartBrokenSession = FiveSteamConstants.k_nSteamNetworkingSend_AutoRestartBrokenSession
+    }
+}
